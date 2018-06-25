cc5feb5d1d78a1d3e3a8e501a7f7a64038de257b
CristianG <tapcrt@gmail.com>
2018-06-25 07:05:18 -0500

    CG-Correcion borrado de lista completa y estructura

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,5 +1,5 @@
 class TodoListsController < ApplicationController
-  before_action :set_todo_list, only: [:show, :edit, :update, :destroy]
+  before_action :set_todo_list, only: %i[show edit update destroy]
 
   # GET /todo_lists
   # GET /todo_lists.json
@@ -9,8 +9,7 @@ class TodoListsController < ApplicationController
 
   # GET /todo_lists/1
   # GET /todo_lists/1.json
-  def show
-  end
+  def show; end
 
   # GET /todo_lists/new
   def new
@@ -18,8 +17,7 @@ class TodoListsController < ApplicationController
   end
 
   # GET /todo_lists/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /todo_lists
   # POST /todo_lists.json
@@ -62,13 +60,14 @@ class TodoListsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_todo_list
-      @todo_list = TodoList.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def todo_list_params
-      params.require(:todo_list).permit(:titulo, :descripcion)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_todo_list
+    @todo_list = TodoList.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def todo_list_params
+    params.require(:todo_list).permit(:titulo, :descripcion)
+  end
 end
--- db/migrate/20180623124535_create_todo_items.rb
+++ db/migrate/20180623124535_create_todo_items.rb
@@ -2,7 +2,7 @@ class CreateTodoItems < ActiveRecord::Migration[5.2]
   def change
     create_table :todo_items do |t|
       t.string :contenido
-      t.references :todo_list, index: true # Tengo duda llave foranea
+      t.references :todo_list, index: true # Tengo duda llave principal
 
       t.timestamps
     end
